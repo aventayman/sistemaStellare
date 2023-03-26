8529942a28be87edba283aa0f6eae9399c51ed4c
Ayman <aventayman@yahoo.it>
2023-03-26 15:32:53 +0200

    Aggiunta e rimozione di lune e pianeti all'interno del sistema

--- src/it/unibs/fp/sistemaStellare/SistemaStellare.java
+++ src/it/unibs/fp/sistemaStellare/SistemaStellare.java
@@ -2,7 +2,7 @@ package it.unibs.fp.sistemaStellare;
 
 public class SistemaStellare {
     private String nome;
-    private Posizione baricentro = new Posizione();
+    private Posizione baricentro = new Posizione(0, 0);
     private Stella stella = new Stella();
 
     public SistemaStellare(String nome, Stella stella) {
@@ -12,5 +12,37 @@ public class SistemaStellare {
 
     public Stella getStella() {
         return stella;
+    }
+
+    public void aggiungiPianeta(Pianeta pianeta) {
+        //Aggiunta del pianeta
+        stella.getListaPianeti().add(pianeta);
+
+        //Aggiornamento del baricentro del sistema
+        baricentro = CalcoloBaricentro.calcolaBaricentro(this);
+    }
+
+    public void aggiungiSatellite(Satellite satellite, Pianeta pianeta) {
+        //Aggiunta della luna
+        pianeta.getListaSatelliti().add(satellite);
+
+        //Aggiornamento del baricentro del sistema
+        baricentro = CalcoloBaricentro.calcolaBaricentro(this);
+    }
+
+    public void rimuoviPianeta(Pianeta pianeta) {
+        //Rimozione del pianeta
+        stella.getListaPianeti().remove(pianeta);
+
+        //Aggiornamento del baricentro del sistema
+        baricentro = CalcoloBaricentro.calcolaBaricentro(this);
+    }
+
+    public void rimuoviSatellite(Satellite satellite, Pianeta pianeta) {
+        //Rimozione della luna
+        pianeta.getListaSatelliti().remove(satellite);
+
+        //Aggiornamento del baricentro del sistema
+        baricentro = CalcoloBaricentro.calcolaBaricentro(this);
     }
 }
